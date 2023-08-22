c5515e72ca2e5afc2539dad797f96721e220ecd8
Manu Krishnan K <manukrishnankt@gmail.com>
2023-08-22 13:56:19 +0530

    info message removal

--- libs/rule-builder/src/lib/rule-builder/rule-builder.component.ts
+++ libs/rule-builder/src/lib/rule-builder/rule-builder.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-inferrable-types */
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
