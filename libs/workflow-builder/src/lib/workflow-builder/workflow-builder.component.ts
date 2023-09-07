4515b69c49c05fb18d619cd81d2524dbdeeefcef
Manu Krishnan K <manukrishnankt@gmail.com>
2023-09-07 14:01:27 +0530

    builder components

---
+++ libs/workflow-builder/src/lib/workflow-builder/workflow-builder.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mk-workspace-workflow-builder',
+  templateUrl: './workflow-builder.component.html',
+  styleUrls: ['./workflow-builder.component.scss'],
+})
+export class WorkflowBuilderComponent {}
